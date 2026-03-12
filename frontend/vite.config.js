cdcd8360bbf936cbd8d1432d89d22431f612ab16
Abhishek-Jamne <abhishekj0610@gmail.com>
2026-03-12 16:51:34 +0530

    styling fixes on login page

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
       allowedHosts: true,
       proxy: {
         '/api': {
-          target: env.VITE_API_TARGET || 'http://127.0.0.1:8500/',
+          target: env.VITE_API_TARGET || 'http://127.0.0.1:8000/',
           changeOrigin: true,
         },
       },
